d4d5e58f41f6b75014bbb40785b7cb760c4ee10a
DESKTOP-KKQ64SH\LG PC <hajh5447@naver.com>
2022-08-23 01:43:49 +0900

    차악

--- src/main/java/com/backend/hanghaew7t4clone/card/CardRepository.java
+++ src/main/java/com/backend/hanghaew7t4clone/card/CardRepository.java
@@ -2,11 +2,21 @@ package com.backend.hanghaew7t4clone.card;
 
 import com.backend.hanghaew7t4clone.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
   List<Card> findAllByMember(Member member);
+
+  @Query("select m from Card m join fetch m.member order by m.createdAt DESC ")
   List<Card> findAllByOrderByCreatedAtDesc();
+
+  @Query("select m from Card m join fetch m.commentListDto order by m.createdAt DESC ")
+  List<Card> findAllByOrderByCreatedAt();
+
+  @Query("select m from Card m join fetch m.imgUrlList order by m.createdAt DESC ")
+  List<Card> findAllByOrderByModifiedAt();
+
 
 }
