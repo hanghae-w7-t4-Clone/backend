f26be351aa0fe07cb3d02731086c6a48f40bf855
ParkJong-ic <whddlrs@naver.com>
2022-08-22 16:24:41 +0900

    댓글, 대댓글에 Like 기본값 0으로 입력

--- src/main/java/com/backend/hanghaew7t4clone/comment/Comment.java
+++ src/main/java/com/backend/hanghaew7t4clone/comment/Comment.java
@@ -22,6 +22,9 @@ public class Comment {
     @Column(nullable = false)
     private String content;
 
+    @Column
+    private int likeCount;
+
     @JoinColumn(name = "member_id", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
@@ -33,9 +36,6 @@ public class Comment {
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "comment", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<ReComment> reCommentList;
 
-    @Column
-    private int likeCount;
-
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "comment", cascade = CascadeType.ALL)
     private List<Likes> likesList;
 
@@ -43,14 +43,16 @@ public class Comment {
         this.content = content;
         this.member = member;
         this.card = card;
+        this.likeCount = 0;
     }
 
     public CommentResponseDto getAllCommentDto() {
         return CommentResponseDto.builder()
                 .id(this.id)
                 .profilePhoto(this.getMember().getProfilePhoto())
-                .content(this.content)
                 .nickname(this.getMember().getNickname())
+                .content(this.content)
+                .likeCount(this.likeCount)
                 .build();
     }
 
