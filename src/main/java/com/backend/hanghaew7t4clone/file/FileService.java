5025457be95cd4bf9eb6a248f57a7f124e4adce0
DESKTOP-KKQ64SH\LG PC <hajh5447@naver.com>
2022-08-24 13:11:10 +0900

    누락한 코드 삽입

--- src/main/java/com/backend/hanghaew7t4clone/file/FileService.java
+++ src/main/java/com/backend/hanghaew7t4clone/file/FileService.java
@@ -7,6 +7,7 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.backend.hanghaew7t4clone.exception.CustomException;
 import com.backend.hanghaew7t4clone.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,17 +22,18 @@ public class FileService {
    @Value("${bucketName}")
    private String S3Bucket; // Bucket 이름
 
-
+   @Autowired
    AmazonS3Client amazonS3Client;
+
    public List<String> getImgUrlList(MultipartFile[] multipartFileList) throws IOException {
       List<String> imagePathList = new ArrayList<>();
 
-      for(MultipartFile multipartFile: multipartFileList) {
+      for (MultipartFile multipartFile : multipartFileList) {
          String originalName = multipartFile.getOriginalFilename(); // 파일 이름
          long size = multipartFile.getSize();  // 파일 크기
          String type = multipartFile.getContentType();
-         if(!type.startsWith("image")) throw new CustomException(ErrorCode.FILE_TYPE_INVALID);
-         if(size>3500000) throw new CustomException(ErrorCode.FILE_SIZE_INVALID);
+         if (!type.startsWith("image")) throw new CustomException(ErrorCode.FILE_TYPE_INVALID);
+         if (size > 3500000) throw new CustomException(ErrorCode.FILE_SIZE_INVALID);
 
          ObjectMetadata objectMetaData = new ObjectMetadata();
          objectMetaData.setContentType(multipartFile.getContentType());
