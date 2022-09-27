efd7c36f57f394c2378091967412193960e391e3
ParkJong-ic <whddlrs@naver.com>
2022-09-27 17:25:35 +0900

    style: Git Actions 실험용

--- src/main/java/com/backend/hanghaew7t4clone/domain/comment/Comment.java
+++ src/main/java/com/backend/hanghaew7t4clone/domain/comment/Comment.java
@@ -8,6 +8,7 @@ import com.backend.hanghaew7t4clone.global.shared.Timestamped;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
 import javax.persistence.*;
 import java.util.List;
 import java.util.Set;
